219cde378460ed7e2ed5e6cd76dd0596167dbe92
dmichura <dejwidcode@gmail.com>
2025-11-29 00:12:55 +0100

    Create printArray void

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+#include <vector>
+#include "MergeSorter.h"
 
 using namespace std;
+
+template <typename T>
+void printArray(const vector<T>& arr) {
+    for (const auto& val : arr) {
+        cout << val << " ";
+    }
+    cout << endl;
+}
 
 int main() {
     cout << "Hello world\n";
