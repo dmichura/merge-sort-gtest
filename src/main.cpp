be0f9d0d807afb36f26b601641f3e41001de2cc5
dmichura <dejwidcode@gmail.com>
2025-11-29 00:14:13 +0100

    Check sort

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,18 @@ void printArray(const vector<T>& arr) {
 }
 
 int main() {
-    cout << "Hello world\n";
+    vector<int> intTab = { 12, -5, 0, 7, 3, 12, -1, 49, 31 };
+    MergeSorter<int> intSorter;
+    
+    printArray(intTab);
+    intSorter.sort(intTab);
+    printArray(intTab);
+
+    vector<double> doubleTab = { 3.14, -1.1, 0.0, 2.5, 3.14, -0.5, -4.4, 5.96 };
+    MergeSorter<double> doubleSorter;
+    
+    printArray(doubleTab);
+    doubleSorter.sort(doubleTab);
+    printArray(doubleTab);
     return 0;
 }
